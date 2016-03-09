39bcf30c67ccc95874e3304bc24a7fa8128bbd02
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-09 14:07:10 +0100

    first trials

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+
+
+//import static javafx.application.Application.*;
+
+import javafx.stage.Stage;
+
 /**
  * Created by jvdwi on 9-3-2016.
  */
-public class main {
+public class main extends javafx.application.Application {
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("IntelliJ is slechter dan Netbeans :P");
+    }
 }
