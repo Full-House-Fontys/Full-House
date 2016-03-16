0e78ad4fa6da5bc1cff01afe4b02fe15acb1157b
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-16 13:25:30 +0100

    delete

---
+++ src/DA/DBDelete.java
@@ -0,0 +1,13 @@
+package DA;
+
+import DBElements.Material;
+
+import java.sql.Connection;
+
+/**
+ * Created by jvdwi on 16-3-2016.
+ */
+public class DBDelete {
+    public static void deleteValue(Connection conn, String materiaal, T object) {
+    }
+}
