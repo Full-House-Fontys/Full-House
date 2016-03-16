1992770ac0810d9a3cfaece0544577b6fe50a359
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-16 08:56:12 +0100

    not more necessairy
    because it goes into another class

--- src/main.java
+++ src/main.java
@@ -17,29 +17,28 @@ public class main extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try {
-
-            String userName = "dbi329146";
-            String passWord = "Fullhouseaapje";
-            String url = "jdbc:sqlserver://mssql.fhict.local";
-
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection conn = DriverManager.getConnection(url, userName, passWord);
-
-            System.out.println("connected");
-
-            Statement st = conn.createStatement();
-            String queryString = "SELECT * FROM FULLHOUSE";
-            ResultSet rs = st.executeQuery(queryString);
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-            conn.close();
-        } catch (SQLException ex){
-            System.out.println("Connectie is niet gemaakt");
-        }
-        finally{
-            System.exit(0);
-        }
+//        try {
+//            String userName = "dbi329146";
+//            String passWord = "Fullhouseaapje";
+//            String url = "jdbc:sqlserver://mssql.fhict.local";
+//
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            Connection conn = DriverManager.getConnection(url, userName, passWord);
+//
+//            System.out.println("connected");
+//
+//            Statement st = conn.createStatement();
+//            String queryString = "SELECT * FROM FULLHOUSE";
+//            ResultSet rs = st.executeQuery(queryString);
+//            while (rs.next()) {
+//                System.out.println(rs.getString(1));
+//            }
+//            conn.close();
+//        } catch (SQLException ex){
+//            System.out.println("Connectie is niet gemaakt");
+//        }
+//        finally{
+//            System.exit(0);
+//        }
     }
 }
