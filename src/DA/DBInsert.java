ebccbec284118019f6915ff953d894cc6ef17103
Skaj7 <kaj753@hotmail.com>
2016-03-16 09:25:38 +0100

    Database Access begin

---
+++ src/DA/DBInsert.java
@@ -0,0 +1,26 @@
+package DA;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by kaj75 on 15-3-2016.
+ */
+public class DBInsert {
+    static void closeAll(PreparedStatement ps, ResultSet rs) {
+        try {
+            rs.close();
+        } catch (Exception e) {
+        }
+        try {
+            ps.close();
+        } catch (Exception e) {
+        }
+    }
+
+    static <T> void insertValue(Connection conn, String tableName, T object)  {
+        System.out.println(object.toString());
+    }
+
+}
--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@
 
 //import static javafx.application.Application.*;
 
+import DA.DBConnection;
 import javafx.stage.Stage;
 
 import java.sql.*;
@@ -10,6 +11,7 @@ import java.sql.*;
  * Created by jvdwi on 9-3-2016.
  */
 public class main extends javafx.application.Application {
+
     public static void main(String[] args) {
         launch(args);
 
@@ -17,28 +19,10 @@ public class main extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        try {
-//            String userName = "dbi329146";
-//            String passWord = "Fullhouseaapje";
-//            String url = "jdbc:sqlserver://mssql.fhict.local";
-//
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            Connection conn = DriverManager.getConnection(url, userName, passWord);
-//
-//            System.out.println("connected");
-//
-//            Statement st = conn.createStatement();
-//            String queryString = "SELECT * FROM FULLHOUSE";
-//            ResultSet rs = st.executeQuery(queryString);
-//            while (rs.next()) {
-//                System.out.println(rs.getString(1));
-//            }
-//            conn.close();
-//        } catch (SQLException ex){
-//            System.out.println("Connectie is niet gemaakt");
-//        }
-//        finally{
-//            System.exit(0);
-//        }
+        try {
+            DBConnection dbConnection = new DBConnection();
+        } catch (SQLException ex){
+            System.out.println("Connectie is niet gemaakt");
+        }
     }
 }
