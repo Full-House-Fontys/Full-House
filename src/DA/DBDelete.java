efb94299b9f957c657343d10a1d6f541492d2d94
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-16 13:35:03 +0100

    update and delete fixed

--- src/DA/DBDelete.java
+++ src/DA/DBDelete.java
@@ -1,13 +1,11 @@
 package DA;
 
-import DBElements.Material;
-
 import java.sql.Connection;
 
 /**
  * Created by jvdwi on 16-3-2016.
  */
 public class DBDelete {
-    public static void deleteValue(Connection conn, String materiaal, T object) {
+    public static <T> void deleteValue(Connection conn, String materiaal, T object) {
     }
 }
--- src/DA/DBUpdate.java
+++ src/DA/DBUpdate.java
@@ -1,13 +1,11 @@
 package DA;
 
-import DBElements.Material;
-
 import java.sql.Connection;
 
 /**
  * Created by jvdwi on 16-3-2016.
  */
 public class DBUpdate {
-    public static void updateValue(Connection conn, String materiaal, T object) {
+    public static <T> void updateValue(Connection conn, String materiaal, T object) {
     }
 }
