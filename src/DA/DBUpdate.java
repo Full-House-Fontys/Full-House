78ac19671fe4ad21072e4ec55dfca9ecd8c886f7
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-16 13:25:05 +0100

    update and delete

---
+++ src/DA/DBUpdate.java
@@ -0,0 +1,13 @@
+package DA;
+
+import DBElements.Material;
+
+import java.sql.Connection;
+
+/**
+ * Created by jvdwi on 16-3-2016.
+ */
+public class DBUpdate {
+    public static void updateValue(Connection conn, String materiaal, T object) {
+    }
+}
