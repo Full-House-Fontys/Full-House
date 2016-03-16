5be428ef8834303765d8a813045804984ccf88fc
WALUIGI96 <jvdwijgert@hotmail.com>
2016-03-16 13:03:10 +0100

    made first test

---
+++ test/MaterialManageTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jvdwi on 16-3-2016.
+ */
+public class MaterialManageTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRenewMaterials() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertMaterial() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateMaterial() throws Exception {
+
+    }
+}
