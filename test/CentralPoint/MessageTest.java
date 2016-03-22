9718e5d5b7e60c1061168f2555ce11ef614902fc
Skaj7 <kaj753@hotmail.com>
2016-03-22 18:07:33 +0100

    voor alle classen een test classen toegevoegd

---
+++ test/CentralPoint/MessageTest.java
@@ -0,0 +1,33 @@
+package CentralPoint;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kaj Suiker on 22-3-2016.
+ */
+public class MessageTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTitel() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMessage() throws Exception {
+
+    }
+}
---
+++ test/ShutdownHookTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kaj Suiker on 22-3-2016.
+ */
+public class ShutdownHookTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRun() throws Exception {
+
+    }
+}
