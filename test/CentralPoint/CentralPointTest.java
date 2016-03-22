82350cad92e68b8e621504b591938fb83c09d8dc
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-03-22 10:38:06 +0100

    unittests for centrail point made

---
+++ test/CentralPoint/CentralPointTest.java
@@ -0,0 +1,58 @@
+package CentralPoint;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jvdwi on 22-3-2016.
+ */
+public class CentralPointTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testStaffOnLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStaffOnLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMaterials() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertMaterial() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMaterialById() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateMaterial() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteMaterial() throws Exception {
+
+    }
+}
