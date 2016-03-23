069a3a6ffb18fb7d977782802caf4ea910b40f4e
Qunfong <Qunfong@live.nl>
2016-03-23 10:20:29 +0100

    made DaoMission

--- src/GUI/fireController.java
+++ src/GUI/fireController.java
@@ -1,30 +1,61 @@
 package GUI;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import CentralPoint.Mission;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
- * Created by Qunfo on 21-3-2016.
+ * Created by Qunfo on 23-3-2016.
  */
-public class fireController implements Runnable {
+public class fireController {
+    @FXML
+    private Label
+            lbReceived;
 
+    public fireController() {
 
-
-    @Override
-    public void run() {
-        Stage primaryStage = new Stage();
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/TaskV1.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 900, 650));
-        primaryStage.show();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+               receiveSocket();
+            }
+        },0,1000);
     }
+
+    private void receiveSocket() {
+        int port = 2002;
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            Socket s = ss.accept();
+            InputStream is = s.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            ArrayList<Mission> missions = (ArrayList<Mission>) ois.readObject();
+            if (missions != null) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        lbReceived.setText("Received Missions from server");
+                    }
+                });
+            }
+            is.close();
+            s.close();
+            ss.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
