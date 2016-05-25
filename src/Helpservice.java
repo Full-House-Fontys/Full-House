8a8bd215508506e639c34d4fb4cbbe495952a62e
MarkClaessens <mark-claessens@hotmail.com>
2016-05-25 14:27:27 +0200

    venster toegevoegd

--- src/Helpservice.java
+++ src/Helpservice.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Hupldienst extends Application {
+public class Helpservice extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -17,7 +17,7 @@ public class Hupldienst extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Hulpdienst.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Helpservice.fxml"));
         primaryStage.setTitle("Hulpdienst");
         primaryStage.setScene(new Scene(root, 619, 466));
         primaryStage.show();
