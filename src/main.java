f02434efea9df8569e9d2a3323534de6a6c687da
MarkClaessens <mark-claessens@hotmail.com>
2016-06-01 14:53:22 +0200

    engelse

--- src/main.java
+++ src/main.java
@@ -26,7 +26,7 @@ public class main extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Central Point Program");
         primaryStage.setScene(new Scene(root, 900, 650));
         primaryStage.show();
     }
