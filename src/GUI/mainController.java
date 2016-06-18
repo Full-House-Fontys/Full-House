4136e86a90cc4808afcf316dd9606b894ab2b4b0
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-06-18 19:31:15 +0200

    more blended color on selection

--- src/GUI/mainController.java
+++ src/GUI/mainController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
 
 import java.io.IOException;
